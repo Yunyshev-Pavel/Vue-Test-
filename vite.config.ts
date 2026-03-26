f50ac6ca41a09ced5ba41a5f4d87fa352738b5cf
Pavel Ynyshev <paha020798@mail.ru>
2026-03-26 15:30:26 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/testvue/",
+  base:process.env.VERCEL ? "/" : "/testvue/",
   test: {
     globals: true,
     environment: 'jsdom',
