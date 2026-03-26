f927e001c4feba1a9ef01757bca2b7ef3a8deb6b
Pavel Ynyshev <paha020798@mail.ru>
2026-03-26 15:42:18 +0700

    fix

--- src/constants/dynamicImages.ts
+++ src/constants/dynamicImages.ts
@@ -1,15 +1,15 @@
 export const DYNAMIC_IMAGES = {
   MEASURE: Array.from(
     { length: 9 },
-    (_, i) => `/images/measure/power_${i}.png`,
+    (_, i) => `../../public/images/measure/power_${i}.png`,
   ),
   ROBOT: {
-    DEFAULT: '/images/robot/robot_1.png',
-    WINNER: '/images/robot/robot_3.png',
-    LOOSER: '/images/robot/robot_2.png',
+    DEFAULT: '../../public/images/robot/robot_1.png',
+    WINNER: '../../public/images/robot/robot_3.png',
+    LOOSER: '../../public/images/robot/robot_2.png',
   },
   BUTTON: {
-    ACTIVE: '/images/button/button_active.png',
-    INACTIVE: '/images/button/button.png',
+    ACTIVE: '../../public/images/button/button_active.png',
+    INACTIVE: '../../public/images/button/button.png',
   },
 } as const
